9f6b1ad8579367660b6cfd63941b4e23fc2756e7
swathimuroor <swathimuroor@gmail.com>
2021-02-17 13:09:40 +1100

    add testing solutions

--- src/buyBeer/index.js
+++ src/buyBeer/index.js
@@ -12,6 +12,6 @@ const canGregBuyBeer = () => {
   if (canBuyBeer(greg.age)) {
     return `Greg is ${greg.age}, he can buy beer!`;
   } else {
-    return `Greg is ${greg.age}, he can't buy beer :(`;
+    return `Greg is ${greg.age}, he can not buy beer :(`;
   }
 };
